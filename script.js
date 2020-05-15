1624bd4ffd130ccbc0a98e66614992b1c3f93596
dnorhoj <daniel.norhoj@gmail.com>
2020-05-15 14:38:48 +0200

    Been working a lot of time on this now, don't think it's worth it anymore

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // like random.randint() from python
 function randint(x, y) {
-    return Math.floor(Math.random()*(y-x))+x;
+    return Math.floor(Math.random() * (y - x)) + x;
 }
 
 // Calculate numbers, etc.
@@ -8,17 +8,17 @@ function createTiles(width, height, bombs) {
     // Create tile template grid
     let tiles = [];
 
-    for (y=0; y<height; y++) {
+    for (y = 0; y < height; y++) {
         let row = [];
 
-        for (x=0; x<width; x++) {
+        for (x = 0; x < width; x++) {
             row.push(false);
         }
         tiles.push(row);
     }
 
     // Put in the bombs
-    for (_=0; _<bombs; _++) {
+    for (_ = 0; _ < bombs; _++) {
         let x;
         let y;
         do {
@@ -30,18 +30,18 @@ function createTiles(width, height, bombs) {
     }
 
     // Put in the numbers
-    for(y=0; y<height; y++) {
-        for(x=0; x<width; x++) {
-            if(tiles[y][x] !== "x") {
+    for (y = 0; y < height; y++) {
+        for (x = 0; x < width; x++) {
+            if (tiles[y][x] !== "x") {
                 let surrounding = 0;
                 // Check surrounding spaces for bombs
-                for(y1=-1; y1<=1; y1++) {
-                    for(x1=-1; x1<=1; x1++) {
+                for (y1 = -1; y1 <= 1; y1++) {
+                    for (x1 = -1; x1 <= 1; x1++) {
                         try {
-                            if(tiles[y+y1][x+x1] === "x") {
+                            if (tiles[y + y1][x + x1] === "x") {
                                 surrounding++;
                             }
-                        } catch (TypeError) {}
+                        } catch (TypeError) { }
                     }
                 }
                 tiles[y][x] = surrounding;
@@ -56,16 +56,16 @@ function createTiles(width, height, bombs) {
 function createGrid(selector, width, height) {
 
     // Create rows
-    for (y=0; y<height; y++) {
+    for (y = 0; y < height; y++) {
         let row = $("<tr></tr>");
 
         // Create cells
-        for (x=0; x<width; x++) {
+        for (x = 0; x < width; x++) {
             let cell = $("<td></td>").attr("x", x).attr("y", y).addClass("unclicked");
             $(row).append(cell);
         }
 
-        $(selector).append(row);
+        selector.append(row);
     }
 }
 
@@ -74,40 +74,59 @@ $(document).ready(() => {
     let height = 14;
     let bombs = 40;
 
+    let table = $("#grid");
     let tiles = createTiles(width, height, bombs);
-    createGrid("#grid", width, height);
+    let gameover = false;
+    createGrid(table, width, height);
 
     let click = event => {
         let t = $(event.target);
         let x = t.attr("x");
         let y = t.attr("y");
-
         let tile = tiles[y][x];
 
         t.off('click');
         t.off("contextmenu")
         t.removeAttr("class");
-        t.text(tile);
 
-        if(tile === 0) {
-            for(y1=-1; y1<=1; y1++) {
-                for(x1=-1; x1<=1; x1++) {
-                    let x2 = Number(t.attr("x"))+x1;
-                    let y2 = Number(t.attr("y"))+y1;
+        if (tile === 0) {
+            for (y1 = -1; y1 <= 1; y1++) {
+                for (x1 = -1; x1 <= 1; x1++) {
+                    let x2 = Number(t.attr("x")) + x1;
+                    let y2 = Number(t.attr("y")) + y1;
 
-                    try {
-                        $(".unclicked[x="+x2+"][y="+y2+"]").click()
-                    } catch(TypeError) {}
+                    setTimeout(() => {
+                        try {
+                            table.children()[y2].children[x2].click();
+                        } catch (TypeError) { }
+                    }, 1);
                 }
             }
+            return;
+        } else if (tile === "x") {
+            t.addClass("bomb");
+            if (!gameover) {
+                $(".flag").contextmenu();
+                let unclicked = $(".unclicked");
+                gameover = true;
+                unclicked.each(i => {
+                    setTimeout(() => {
+                        unclicked[i].click();
+                    }, 100);
+                });
+            }
+            return;
         }
-        console.log("Click! %c("+t.attr("x")+", "+t.attr("y")+")", "color: green");
+
+        t.text(tile);
+        //console.log("Click on coord: %c("+t.attr("x")+", "+t.attr("y")+")", "color: green");
     }
 
     let rightclick = event => {
         let t = $(event.target);
+        event.preventDefault();
 
-        if(t.attr("class") === "unclicked") {
+        if (t.attr("class") === "unclicked") {
             t.attr("class", "flag");
             t.off("click");
         } else {
@@ -120,5 +139,5 @@ $(document).ready(() => {
 
     $("td").on("click", click);
     $("td").on("contextmenu", rightclick);
-    $("table").on("contextmenu", () => {return false})
+    $("table").on("contextmenu", () => { return false })
 });
